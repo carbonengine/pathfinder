f1b0a990e45d224106d7752d5088fdfc157e03fc
ccptoebeans <ccptoebeans@users.noreply.github.com>
2026-08-06 10:51:00 +0000

    use GITHUB_CARBON_PAT auth token for PR and commit status publisher features

--- .teamcity/MacOS/Project.kt
+++ .teamcity/MacOS/Project.kt
@@ -152,7 +152,7 @@ class CarbonBuildMacOS(buildName: String, configType: String, preset: String, ag
             vcsRootExtId = "${DslContext.settingsRootId.id}"
             provider = github {
                 authType = token {
-                    token = "%GITHUB_TEAMCITY_TOKEN%"
+                    token = "%GITHUB_CARBON_PAT%"
                 }
                 filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
             }
@@ -161,7 +161,7 @@ class CarbonBuildMacOS(buildName: String, configType: String, preset: String, ag
             publisher = github {
                 githubUrl = "https://api.github.com"
                 authType = personalToken {
-                    token = "%GITHUB_TEAMCITY_TOKEN%"
+                    token = "%GITHUB_CARBON_PAT%"
                 }
             }
         }
--- .teamcity/Windows/Project.kt
+++ .teamcity/Windows/Project.kt
@@ -207,7 +207,7 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String, 
             vcsRootExtId = "${DslContext.settingsRootId.id}"
             provider = github {
                 authType = token {
-                    token = "%GITHUB_TEAMCITY_TOKEN%"
+                    token = "%GITHUB_CARBON_PAT%"
                 }
                 filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
             }
@@ -216,7 +216,7 @@ class CarbonBuildWindows(buildName: String, configType: String, preset: String, 
             publisher = github {
                 githubUrl = "https://api.github.com"
                 authType = personalToken {
-                    token = "%GITHUB_TEAMCITY_TOKEN%"
+                    token = "%GITHUB_CARBON_PAT%"
                 }
             }
         }
